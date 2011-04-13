f100cdac74dda8e2338f975911ffcc605f18a790
Jason Kraus <jasonk@cukerinteractive.com>
2011-04-13 12:29:56 -0700

    ensure table is myisam

--- haystackmyisam/sql/searchableobject.sql
+++ haystackmyisam/sql/searchableobject.sql
@@ -1,1 +1,2 @@
-ALTER TABLE haystackmyisam_searchableobject ADD FULLTEXT(search_text); 
+ALTER TABLE `haystackmyisam_searchableobject` ENGINE=MyISAM;
+ALTER TABLE `haystackmyisam_searchableobject` ADD FULLTEXT(search_text); 
